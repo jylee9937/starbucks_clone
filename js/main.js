25e11eb62e2744c0a6bb6ad1031467ada2c44dcd
jylee9937 <48907146+jylee9937@users.noreply.github.com>
2021-03-16 10:39:24 +0900

    Header_subMenu&searchBar

--- js/main.js
+++ js/main.js
@@ -4,4 +4,17 @@ const searchInputEl = searchEl.querySelector('input');
 searchEl.addEventListener('click', function(){
   //logic..
   searchInputEl.focus();
-})
+});
+// 원래 input요소만을 focus할 수 있는데, 자바스크립트로 강제로 돋보기를 눌러도 focus가 되도록 바꿈
+
+searchInputEl.addEventListener('focus', function(){
+  //Logic..
+  searchInputEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function(){
+  //Logic..
+  searchInputEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
